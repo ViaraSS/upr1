45bd298f4851d6222dbd040bb34f5cad164f3910
Viara Sivova <vsivova@tu-sofia.bg>
2024-03-05 16:28:17 +0200

    upr 2

---
+++ WwlcomeExtended/Loggers/LoggerProvider.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using WwlcomeExtended.Loggers;
+
+namespace WelcomeExtended.Loggers
+{
+    public class LoggerProvider : ILoggerProvider
+    {
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new HashLogger(categoryName);
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
